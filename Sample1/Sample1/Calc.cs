9fecd882884d87a83ba6c1a88db3c94345b2be4a
karoly.vass <karoly.vass@SWS00503>
2017-01-12 16:23:46 +0100

    Commit for point 14

--- Sample1/Sample1/Calc.cs
+++ Sample1/Sample1/Calc.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sample1
+﻿namespace Sample1
 {
     public class Calc
     {
@@ -20,6 +14,11 @@ namespace Sample1
         public double DivB(int a, int b)
         {
             return (double)a / b;
+        }
+
+        public double DivD(int a, int b)
+        {
+            return (double)a / (double)b;
         }
 
     }
