a09e8a67ba3719b6cff77979c094435327030172
karoly.vass <karoly.vass@SWS00503>
2017-01-12 16:43:25 +0100

    Commit for point 34

--- Sample1/Sample1/Book.cs
+++ Sample1/Sample1/Book.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Sample1
 {
-    class Book
+    public class Book
     {
+        public int Length { get; set; }
     }
 }
